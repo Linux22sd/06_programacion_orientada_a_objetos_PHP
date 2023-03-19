6941398750ef89d1db4024c35d127f3f1a45d4ab
Luis Padilla <alexloudog@hotmail.com>
2023-03-18 23:41:53 -0600

    Definicion de modularidad

---
+++ 06_Modularidad.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Modularidad es la base de un buen codigo, dividir el codigo ser lo mas especifico posible
+ * y siempre tener un codogo mas organizado, mantenible y facil de entender
+ * Basicamente crear varios modulos de codigo cada uno de ellos en lo que se 
+ * especializa como vistas, funciones, controladores etc.
+ */
+
+
+?>
