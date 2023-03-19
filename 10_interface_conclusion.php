8626b039d76a1d943ffe5a9cb9cb24e50faea16e
Luis Padilla <alexloudog@hotmail.com>
2023-03-19 01:38:49 -0600

    Interface conclusion

---
+++ 10_interface_conclusion.php
@@ -0,0 +1,21 @@
+<?php
+
+interface Person{
+    public function getName();
+}
+
+class Admin implements Person{
+
+    public $name;
+
+    public function __construct($name){
+        $this->name = $name;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+}
+$admin = new Admin('Luis');
+echo $admin->getName();
+?>
